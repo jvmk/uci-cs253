81625fe9f3d7c3b59e0a8a27ef6cc75cb9bbb28a
Janus Varmarken <varmarken@gmail.com>
2018-10-02 23:14:04 -0700

    Streamline code by doing everything using Streams (pun intended)

--- Week1/TermFreqWeek01.java
+++ Week1/TermFreqWeek01.java
@@ -10,16 +10,19 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.stream.Stream;
-
-
-// NOTE - Professor's ruleset used for splitting:
-// 1 normalize to lowercase
-// 2 replace all non-alphanumeric characters by a space
-// 3 split by space
-// 4 drop all 1-character words
+import java.util.stream.Collectors;
 
 /**
- * Solution for week 1 programming assignment of UCI CS253 (Fall 2018).
+ * <p>Solution for week 1 programming assignment of UCI CS253 (Fall 2018).</p>
+ * <p>
+ * <b>NOTE:</b> Professor Lopes' ruleset used for splitting:
+ * <ol>
+ * <li>normalize to lowercase</li>
+ * <li>replace all non-alphanumeric characters by a space</li>
+ * <li>split by space</li>
+ * <li>drop all words that are less than two characters long</li>
+ * </ol>
+ * </p>
  *
  * @author Janus Varmarken {@literal <jvarmark@uci.edu>}
  */
@@ -30,41 +33,29 @@ public class TermFreqWeek01 {
             System.out.println(usageHint);
             return;
         }
-        final String txtFilePath = args[0];
-        // Read the stop_words.txt file that should reside in the parent dir.
-        // The file is a csv file containing words that should be dropped when computing the term frequencies.
-        List<String> stopWordsLines = Files.readAllLines(Paths.get("../stop_words.txt"));
-        // Store stop words as a Set in order to enable easy exclusion using filters in later code.
-        final Set<String> stopWords = new HashSet<>();
-        for (String line : stopWordsLines) {
-            for (String word : line.split(",")) {
-                if (word.length() > 0) {
-                    stopWords.add(word);
-                }
-            }
+        // Read the stop_words.txt file that should reside in the parent dir, and read the input file.
+        // The stop_words.txt file is a csv file containing words that should be dropped when computing the term frequencies.
+        // Note: use try-with-resource such that the files are automatically closed.
+        try (Stream<String> stopWordsStream = Files.lines(Paths.get("../stop_words.txt")).map(line -> line.split(",")).flatMap(Arrays::stream);
+             Stream<String> inputFileLines = Files.lines(Paths.get(args[0]))) {
+            // Maps a word to its number of occurrences in the input file.
+            final Map<String, Integer> wordCounts = new HashMap<>();
+            // Turn the stopwords stream into a hash set for easy filtering of the input file's content later on.
+            // Note: explicitly collect to HashSet to get the benefit of O(1)-complexity contains()!
+            final Set<String> stopWords = stopWordsStream.collect(Collectors.toCollection(HashSet::new));
+            inputFileLines.map(line -> line.toLowerCase().replaceAll("[^a-zA-Z\\d\\s]", " ")). // Normalize to lower case and convert all non-alphanumeric characters (except whitespace) to a space.
+                    map(line -> line.split("\\s+")).flatMap(Arrays::stream). // Separate into separete words; use flatMap to flatten Stream<String[]> to Stream<String>.
+                    filter(word -> word.length() > 1). // Drop all 1-character words and empty strings.
+                    filter(word -> !stopWords.contains(word)). // Drop all words in stop_words.txt.
+                    forEach(word -> wordCounts.merge(word, 1, (oldVal, newVal) -> oldVal + newVal)); // Count the number of occurrences by filling in the map.
+            // Note: the merge call above associates the entry with a value of 1 if the key is not found; otherwise the
+            // current value is incremented by 1 (as newVal assumes a value of 1).
+
+            // Sort map entry set by value (number of occurrences of the word that is the key) and print the result.
+            // Note that the result of the built-in Integer comparator is negated so as to sort the values in descending order.
+            // We also limit the output to the top-25 words.
+            wordCounts.entrySet().stream().sorted((e1, e2) -> -e1.getValue().compareTo(e2.getValue())).limit(25).
+                    forEach(e -> System.out.println(String.format("%s  -  %d", e.getKey(), e.getValue())));
         }
-        // Maps a word to its number of occurrences in the input file.
-        final Map<String, Integer> wordCounts = new HashMap<>();
-        // Note: use try-with-resource such that the file is automatically closed.
-        try (Stream<String> txtFileLines = Files.lines(Paths.get(txtFilePath))) {
-            // Normalize to lower case and convert all non-alphanumeric characters (except whitespace) to a space.
-            txtFileLines.map(line -> line.toLowerCase().replaceAll("[^a-zA-Z\\d\\s]", " ")).
-                    // Separate into separete words; use flatMap to flatten Stream<String[]> to Stream<String>
-                    map(line -> line.split("\\s+")).flatMap(Arrays::stream).
-                    // Drop all 1-character words and empty strings.
-                    filter(word -> word.length() > 1).
-                    // Drop all words in stop_words.txt, then count the number of occurrences by filling the map.
-                    // The merge call associates the entry with a value of 1 if the key is not found; otherwise the
-                    // current value is incremented by 1 (as newVal assumes a value of 1).
-                    filter(word -> !stopWords.contains(word)).
-                    forEach(word -> wordCounts.merge(word, 1, (oldVal, newVal) -> oldVal + newVal));
-        }
-        // Sort map entry set by value (number of occurrences of the word that is the key) and print the result.
-        // Note that the result of the built-in Integer comparator is negated so as to sort the values in descending order.
-        // We also limit the output to the top-25 words.
-        wordCounts.entrySet().stream().
-                sorted((e1, e2) -> -e1.getValue().compareTo(e2.getValue())).
-                limit(25).
-                forEach(e -> System.out.println(String.format("%s  -  %d", e.getKey(), e.getValue())));
     }
 }
