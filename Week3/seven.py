991dc58f03111bbf3a349931483034dd36b2ce2d
Janus Varmarken <varmarken@gmail.com>
2018-10-20 16:08:46 -0700

    seven.py: directly call Y-combinators in main rather than just replacing the function body with the Y-combinators.

--- Week3/seven.py
+++ Week3/seven.py
@@ -12,31 +12,30 @@ sys.setrecursionlimit(RECURSION_LIMIT+10)
 Y = (lambda h: lambda F: F(lambda x: h(h)(F)(x)))(lambda h: lambda F: F(lambda x: h(h)(F)(x)))
 
 
-def count(word_list, stopwords, wordfreqs):
-    fixed_point = lambda f: lambda wl: lambda sws: lambda wfs: None if wl == [] else f(wl[1:])(sws)(wfs) if wfs.update({wl[0]: (wfs.get(wl[0], 0) + (1 if wl[0] not in sws else 0))}) is None else True
-    Y(fixed_point)(word_list)(stopwords)(wordfreqs)
+# def count(word_list, stopwords, wordfreqs):
+#     fixed_point = lambda f: lambda wl: lambda sws: lambda wfs: None if wl == [] else f(wl[1:])(sws)(wfs) if wfs.update({wl[0]: (wfs.get(wl[0], 0) + (1 if wl[0] not in sws else 0))}) is None else True
+#     Y(fixed_point)(word_list)(stopwords)(wordfreqs)
 
 
-def wf_print_y(wordfreq):
-    Y(lambda f: lambda wfs: None if wfs == []
-      else f(wfs[1:]) if print(f"{wfs[0][0]}  -  {wfs[0][1]}") is None
-      else None)(wordfreq)
+# def wf_print_y(wordfreq):
+#     Y(lambda f: lambda wfs: None if wfs == []
+#       else f(wfs[1:]) if print(f"{wfs[0][0]}  -  {wfs[0][1]}") is None
+#       else None)(wordfreq)
 
-
-# small test case
-# wf_print_y([('a', 1), ('b', 2), ('c', 3)])
 
 stop_words = set(open('../stop_words.txt').read().split(','))
 words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
 word_freqs = {}
-# Theoretically, we would just call count(words, word_freqs)
-# Try doing that and see what happens.
-# count(words, stop_words, word_freqs)
-
 for i in range(0, len(words), RECURSION_LIMIT):
-    count(words[i:i+RECURSION_LIMIT], stop_words, word_freqs)
+    Y(lambda f: lambda wl: lambda sws: lambda wfs: None if wl == []
+      else f(wl[1:])(sws)(wfs) if wfs.update({wl[0]: (wfs.get(wl[0], 0) + (1 if wl[0] not in sws else 0))}) is None
+      else True)(words[i:i+RECURSION_LIMIT])(stop_words)(word_freqs)
+    # count(words[i:i+RECURSION_LIMIT], stop_words, word_freqs)
 
-wf_print_y(sorted(word_freqs.items(), key=operator.itemgetter(1), reverse=True)[:25])
+Y(lambda f: lambda wfs: None if wfs == []
+  else f(wfs[1:]) if print(f"{wfs[0][0]}  -  {wfs[0][1]}") is None
+  else None)(sorted(word_freqs.items(), key=operator.itemgetter(1), reverse=True)[:25])
+# wf_print_y(sorted(word_freqs.items(), key=operator.itemgetter(1), reverse=True)[:25])
 
 
 
