d72d38d8b0b68001d3470aba7ca0cb8e4fbbf583
Janus Varmarken <varmarken@gmail.com>
2018-10-20 11:35:02 -0700

    Eight.java: Cast nested BiConsumer to its raw type to avoid lengthy types in function signatures.

--- Week3/Eight.java
+++ Week3/Eight.java
@@ -16,8 +16,10 @@ import java.util.stream.Stream;
  * In this exercise, we're to obey the "Kick Forward" programming style. This style is essentially a variation of the
  * pipeline style in which functions don't return values, but instead take a second argument that is the next function
  * in the pipeline. The function provided as argument is invoked with what would have been the output of the current
- * function if it was to return a value. Note that this style result in some extremely ugly types for the parameters as
- * we're nesting {@link BiConsumer}s in {@link BiConsumer}s.
+ * function if it was to return a value. Note that this style results in some extremely ugly types for the parameters as
+ * we're nesting {@link BiConsumer}s in {@link BiConsumer}s. To circumvent this issue, I implemented
+ * {@link #simpleType(BiConsumer)} which converts the nested {@code BiConsumer} into its raw type (i.e., it drops all
+ * generic type parameters).
  *
  * @author Janus Varmarken {@literal <jvarmark@uci.edu>}
  */
@@ -39,12 +41,12 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the contents
      *                     of the file.
      */
-    public static void readFile(String filepath, BiConsumer<String, BiConsumer<String, BiConsumer<String, BiConsumer<List<String>, BiConsumer<List<String>, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>>>>>>> nextFunction) {
+    public static void readFile(String filepath, BiConsumer<String, BiConsumer> nextFunction) {
         // Freeing resources: use try-with-resource such that file handles are automatically closed.
         try(Stream<String> lines = Files.lines(Paths.get(filepath))) {
             // Join strings using the system's line separator in order to preserve line breaks in the single resulting
             // string (Files.lines(Path) throws away line separators when reading the file).
-            nextFunction.accept(lines.collect(Collectors.joining(System.lineSeparator())), Eight::normalize);
+            nextFunction.accept(lines.collect(Collectors.joining(System.lineSeparator())), simpleType(Eight::normalize));
         } catch (IOException ioe) {
             // Hide the fact that method is throwing a checked exception by rethrowing it wrapped in an unchecked
             // exception in order to allow this method to be referred to using method references.
@@ -60,10 +62,10 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting string.
      */
-    public static void filterChars(String data, BiConsumer<String, BiConsumer<String, BiConsumer<List<String>, BiConsumer<List<String>, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>>>>>> nextFunction) {
+    public static void filterChars(String data, BiConsumer<String, BiConsumer> nextFunction) {
         // Convert all non-alphanumeric characters (except whitespace) to a space, then invoke 'scan' with the result.
         String alphanumeric = data.replaceAll("[^a-zA-Z\\d\\s]", " ");
-        nextFunction.accept(alphanumeric, Eight::scan);
+        nextFunction.accept(alphanumeric, simpleType(Eight::scan));
     }
 
     /**
@@ -73,8 +75,8 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting string.
      */
-    public static void normalize(String data, BiConsumer<String, BiConsumer<List<String>, BiConsumer<List<String>, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>>>>> nextFunction) {
-        nextFunction.accept(data.toLowerCase(), Eight::removeStopWords);
+    public static void normalize(String data, BiConsumer<String, BiConsumer> nextFunction) {
+        nextFunction.accept(data.toLowerCase(), simpleType(Eight::removeStopWords));
     }
 
     /**
@@ -84,10 +86,10 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting list of words.
      */
-    public static void scan(String data, BiConsumer<List<String>, BiConsumer<List<String>, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>>>> nextFunction) {
+    public static void scan(String data, BiConsumer<List<String>, BiConsumer> nextFunction) {
         // Separate into separate words by splitting for each whitespace.
         List<String> words = Arrays.asList(data.split("\\s+"));
-        nextFunction.accept(words, Eight::frequencies);
+        nextFunction.accept(words, simpleType(Eight::frequencies));
     }
 
     /**
@@ -99,7 +101,7 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting list of words.
      */
-    public static void removeStopWords(List<String> words, BiConsumer<List<String>, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>>> nextFunction) {
+    public static void removeStopWords(List<String> words, BiConsumer<List<String>, BiConsumer> nextFunction) {
         // Read the contents of the stop words file. Use try-with-resource to ensure resources are freed.
         try(Stream<String> swStream = Files.lines(Paths.get("../stop_words.txt"))) {
             // Collect stop words to set. Note: explicitly use HashSet for O(1) complexity contains().
@@ -107,7 +109,7 @@ public class Eight {
                     flatMap(Arrays::stream).collect(Collectors.toCollection(HashSet::new));
             // Drop all one character words and stop words.
             words = words.stream().filter(w -> w.length() > 1 && !stopWords.contains(w)).collect(Collectors.toList());
-            nextFunction.accept(words, Eight::sort);
+            nextFunction.accept(words, simpleType(Eight::sort));
         } catch (IOException ioe) {
             // Hide the fact that method is throwing a checked exception by rethrowing it wrapped in an unchecked
             // exception in order to allow this method to be referred to using method references.
@@ -123,10 +125,10 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting word frequency map.
      */
-    public static void frequencies(List<String> words, BiConsumer<Map<String, Integer>, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>>> nextFunction) {
+    public static void frequencies(List<String> words, BiConsumer<Map<String, Integer>, BiConsumer> nextFunction) {
         final Map<String, Integer> wordFreqs = new HashMap<>();
         words.forEach(word -> wordFreqs.merge(word, 1, (oldVal, newVal) -> oldVal + newVal));
-        nextFunction.accept(wordFreqs, Eight::printText);
+        nextFunction.accept(wordFreqs, simpleType(Eight::printText));
     }
 
     /**
@@ -138,7 +140,7 @@ public class Eight {
      * @param nextFunction The next function in the pipeline, i.e., the function that is to be invoked with the
      *                     resulting sorted list of pairs of words and their frequencies.
      */
-    public static void sort(Map<String, Integer> wordFreqs, BiConsumer<List<Pair<String, Integer>>, Consumer<Void>> nextFunction) {
+    public static void sort(Map<String, Integer> wordFreqs, BiConsumer<List<Pair<String, Integer>>, Consumer<?>> nextFunction) {
         List<Pair<String, Integer>> sorted = wordFreqs.entrySet().stream().
                 sorted((e1, e2) -> -e1.getValue().compareTo(e2.getValue())).
                 map(entry -> new Pair<>(entry.getKey(), entry.getValue())).collect(Collectors.toList());
@@ -178,6 +180,18 @@ public class Eight {
             mItem1 = item1;
             mItem2 = item2;
         }
+    }
+
+    /**
+     * Casts a {@link BiConsumer} with a nested {@code BiConsumer } to a {@code BiConsumer<T, BiConsumer>}, i.e.,
+     * converts the nested {@code BiConsumer} to its raw type.
+     * @param f A {@code BiConsumer} that takes another {@code BiConsumer} as its second argument.
+     * @param <T> The the type of the first argument of the outer {@code BiConsumer}.
+     * @param <B> The full type of the nested {@code BiConsumer}.
+     * @return A reference to {@code f} of type {@code BiConsumer<T, BiConsumer>}.
+     */
+    private static <T, B> BiConsumer<T, BiConsumer> simpleType(BiConsumer<T, B> f) {
+        return (BiConsumer<T, BiConsumer>) f;
     }
 
 }
